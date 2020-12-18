92184350de29410654b448f84f6c0ab1599c9457
felix-oss <uv.mhjl@gmail.com>
2020-12-18 15:12:00 +0000

    initial

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,24 +1,43 @@
 import React from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
-export function Home() {
-	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+export class Home extends React.Component {
+	constructor() {
+		super();
+		this.state = {
+			tasksList: ["Clean up the house"],
+			task: ""
+		};
+	}
+	addList = e => {
+		if (e.keyCode == 13) {
+			var updateTaskList = this.state.tasksList;
+			updateTaskList.push(this.state.task);
+			this.setState({ taskList: updateTaskList });
+		}
+	};
+	render() {
+		return (
+			<div>
+				<div className="d-flex justify-content-center">
+					<h2>Write down a note</h2>
+				</div>
+				<div className="d-flex justify-content-center">
+					<input
+						className="type"
+						onChange={e => this.setState({ task: e.target.value })}
+						value={this.state.task}
+						type="text"
+						onKeyUp={e => this.addList(e)}
+					/>
+				</div>
+				<div className="d-flex justify-content-center">
+					<ol className="list">
+						{this.state.tasksList.map((content, i) => {
+							return <li key={i}>{content}</li>;
+						})}
+					</ol>
+				</div>
+			</div>
+		);
+	}
 }
